38ed4ff5217343f313ef604aed7f8c330ede630a
nazaawa <yetshandedi@gmail>
2021-08-27 06:05:45 +0100

    main file

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/categorie_screens.dart';
+import 'package:flutter_complete_guide/category_meal_screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: "Raleway",
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                body2: TextStyle(
+                  fontSize: 24
+                ),
+                title: TextStyle(fontSize: 24,fontFamily: 'RobotoCondensed')
+              )),
+      home: CategoryScreens(),
+      routes : {
+        CategoryMealScreen.routeName:(ctx)=> CategoryMealScreen(),
+      }
+    );
+  }
+}
